43f7f010799cb5cebcefe4d6bc161110a9217078
pwnzorak <pwnzor.ak@gmail.com>
2014-12-09 14:08:11 -0500

    Python 3
    Updated to Python 3, buffed monster stats

--- dungeon-quest.py
+++ dungeon-quest.py
@@ -1,4 +1,4 @@
-#! python2.7
+#! python3
 #  ________________________________________________________________________________
 # |     ____  __  ___   __________________  _   __   ____  __  ___________________ |
 # |    / __ \/ / / / | / / ____/ ____/ __ \/ | / /  / __ \/ / / / ____/ ___/_  __/ |
@@ -13,7 +13,7 @@ import sys
 import os
 import ctypes
 # change title bar text
-ctypes.windll.kernel32.SetConsoleTitleA("Dungeon Quest")
+ctypes.windll.kernel32.SetConsoleTitleW("Dungeon Quest")
 
 # Start of game
 def start():
@@ -31,20 +31,20 @@ def start():
 	door1 = True
 	player = {'health': 100,
 			  'defence': 10,
-			  'attack': 10,
+			  'attack': 5,
 			  'gold': 0}
-	troll_base = {'health': 70,
+	troll_base = {'health': 80,
 				  'defence': 5,
-				  'attack': 10}
+				  'attack': 25}
 	chest3 = True
 	troll4 = True
-	print """
+	print("""
 	DUNGEON QUEST
 	A Banzai! Production
 	Andrew Karapostolakis 2014
-	"""
+	""")
 	# this does not matter; anything can be input
-	action = raw_input("Press Enter to begin, or type e to exit. ").lower()
+	action = input("Press Enter to begin, or type e to exit. ").lower()
 	# enter first room
 	if action != "e":
 		room1()
@@ -57,55 +57,59 @@ def room1():
 	# no need to say this if user is still in the same room
 	if "Torch" in inventory: # if user has torch, they will have burned down the door
 		# room intro (no door)
-		print "You see an empty room, with the charred frame of the door you burned earlier.\n"
+		print("You see an empty room, with the charred frame of the door you burned earlier.\n")
 	else:
 		# room intro (door)
-		print "\nYou wake up in a dark room. \nYou see a flaming torch and a wooden door. \n"
+		print("\nYou wake up in a dark room. \nYou see a flaming torch and a wooden door. \n")
 	# ask user for action
-	action = raw_input("What do you do? ").lower()
+	action = input("What do you do? ").lower()
 	# loop until player leaves room
 	while action != ("open door", "door", "go through door") and door1 == True:
 		if action in ("open door", "door", "break door", "burn door") and "Torch" in inventory and door1 == True:
-			print "The old, dry, door catches easily and you stand back as it goes up in flames.\n"
+			print("The old, dry, door catches easily and you stand back as it goes up in flames.\n")
 			door1 = False
 		elif action in ("open door", "door", "break door",):
-			print "The door has no handle, and it is too thick to break down. \nIt does seem to be very dry, though.\n"
+			print("The door has no handle, and it is too thick to break down. \nIt does seem to be very dry, though.\n")
 		elif action in ("take torch", "torch", "get torch", "pick up torch"):
-			print "You picked up the torch.\n"
+			print("You picked up the torch.\n")
 			# add torch to inventory
 			inventory.append("Torch")
 		elif action in ("look around", "look", "survey", "look at room"):
 			if door1 == False: # if the door is not there
-				print "An empty room.\n"
+				print("An empty room.\n")
 			elif "Torch" in inventory:
-				print "You see a wooden door.\n"
+				print("You see a wooden door.\n")
 			else:
-				print "You see a flaming torch and a wooden door.\n"
+				print("You see a flaming torch and a wooden door.\n")
+		elif action in ("stats", "health"):
+			print(player)
+		elif action in ("inventory"):
+			print(inventory)
 		else:
-			print "You can't do that here.\n"
+			print("You can't do that here.\n")
 		action = ""
 		# ask user for action again
-		action = raw_input("What do you do? ").lower()
-	print "You walk through the burnt door frame.\n"
+		action = input("What do you do? ").lower()
+	print("You walk through the burnt door frame.\n")
 	# next room
 	room2()
 
 # Second room
 def room2():
 	# room intro
-	print "You see a burnt door frame, a dark doorway, and a wooden door with a handle.\n"
+	print("You see a burnt door frame, a dark doorway, and a wooden door with a handle.\n")
 	# loop until player leaves room
 	loop = True
 	while loop:
 		# ask user for action
-		action = raw_input("What do you do? ").lower()
+		action = input("What do you do? ").lower()
 		if action in ('look around', 'look', 'survey', 'look at room'):
-				print "You see a burnt door frame, a dark doorway, and a wooden door with a handle.\n"
+				print("You see a burnt door frame, a dark doorway, and a wooden door with a handle.\n")
 		elif action in ("door frame", "burnt door", "burnt door frame"):
 			loop = False
 			# enter first room
 			room1()
-		elif action in ("wooden door", "door", "door with handle", "wooden door with handle"):
+		elif action in ("wooden door", "door", "door with handle", "wooden door with handle", "handle"):
 			loop = False
 			# enter third room
 			room3()
@@ -113,8 +117,12 @@ def room2():
 			loop = False
 			# enter fourth room
 			room4()
+		elif action in ("stats", "health"):
+			print(player)
+		elif action in ("inventory"):
+			print(inventory)
 		else:
-			print "You can't do that here.\n"
+			print("You can't do that here.\n")
 
 # Third room
 def room3():
@@ -124,24 +132,28 @@ def room3():
 	# initialize action
 	action = ""
 	# room intro
-	print "You enter a room with a treasure chest.\n"
+	print("You enter a room with a treasure chest.\n")
 	# loop until user leaves room
 	while action not in ("door", "leave", "go through door"):
 		# ask user for action
-		action = raw_input("What do you do? ").lower()
+		action = input("What do you do? ").lower()
 		if action in ("chest", "open chest", "treasure chest", "take treasure chest", "open treasure chest", "take chest", "look in chest", "look in treasure chest") and chest3 == True:
-			print "You found the Leather Armour! You gained 10 defence points.\n"
+			print("You found the Leather Armour! You gained 10 defence points.\n")
 			inventory.append("Leather Armour")
 			# leather armour adds 10 to defence
 			player['defence'] += 10
 			# chest emptied
 			chest3 = False
 		elif action in ("chest", "open chest", "treasure chest", "take treasure chest", "open treasure chest", "take chest", "look in chest", "look in treasure chest"):
-			print "The chest is empty.\n"
+			print("The chest is empty.\n")
 		elif action in ('look around', 'look', 'survey', 'look at room'):
-			print "A room containing only a treasure chest.\n"
+			print("A room containing only a treasure chest.\n")
+		elif action in ("stats", "health"):
+			print(player)
+		elif action in ("inventory"):
+			print(inventory)
 		elif action not in ("door", "leave", "go through door"):
-			print "You can't do that here.\n"
+			print("You can't do that here.\n")
 	room2()
 
 # Fourth room
@@ -154,49 +166,56 @@ def room4():
 	troll = troll_base
 	# room intro
 	if troll4 == True:
-		print "You can see a door, but it's blocked by an ornery troll.\n"
+		print("You can see a door, but it's blocked by an ornery troll.\n")
 	else:
-		print "A room with a door and a dark doorway."
+		print("A room with a door and a dark doorway.")
 	while not (action in ("door", "go through door", "open door") and troll4 == False):
 		# ask user for action
-		action = raw_input("What do you do? ").lower()
-		if action in ("fight troll", "kill troll", "beat up troll", "punch troll in the face", "fight") and troll4 == True:
+		action = input("What do you do? ").lower()
+		if action in ("fight troll", "kill troll", "beat up troll", "punch troll in the face", "fight", "attack") and troll4 == True:
 			while troll4 == True:
-				print "The troll has ", troll['health'], " health. You have ", player['health'], " health. What do you do?"
-				print "ATTACK   FLEE"
-				action = raw_input().lower()
+				print("The troll has ", int(troll['health']), " health. You have ", int(player['health']), " health. What do you do?")
+				print("ATTACK   FLEE")
+				action = input().lower()
 				if action == ("attack"):
 					# torch deals 2 damage (only weapon available at this point)
-					print "\nYou attack the troll and deal ", ((player['attack'] * 2) / troll['defence']), " damage.\n"
+					print("\nYou attack the troll and deal ", int((player['attack'] * 2) / troll['defence']), " damage.\n")
 					troll['health'] -= ((player['attack'] * 2) / troll['defence'])
-					if troll['health'] < 0:
+					if troll['health'] <= 0:
 						troll4 = False
-						print "You defeated the troll! You get 100 gold and a wooden club.\n"
+						print("You defeated the troll! You get 100 gold and a wooden club.\n")
 						inventory.append("Wooden Club")
 						player['gold'] += 100
 						troll4 = False
 					else:
 						# counter-attack (troll attacks with club (5 damage))
-						print "The troll counter-attacks and deals ", ((troll['attack'] * 5) / player['defence']), " damage.\n"
+						print("The troll counter-attacks and deals ", int((troll['attack'] * 5) / player['defence']), " damage.\n")
 						player['health'] -= ((troll['attack'] * 2) / player['defence'])
-						if player['health'] < 0:
-							print "You have died."
-							print "GAME OVER"
+						if player['health'] <= 0:
+							print("You have died.")
+							print("GAME OVER")
 							sys.exit()
 				elif action == ("flee"):
 					break
 				else:
-					print "Please enter either ATTACK or FLEE\n"
+					print("Please enter either ATTACK or FLEE\n")
 		elif action in ('look around', 'look', 'survey', 'look at room') and troll4 == True:
-			print "You can see a door, but it's blocked by an ornery troll.\n"
+			print("You can see a door, but it's blocked by an ornery troll.\n")
+		elif action 
 		elif action in ('look around', 'look', 'survey', 'look at room'):
-			print "A room with a door and a dark doorway."
+			print("A room with a door and a dark doorway.")
 		elif action in ("door", "go through door", "open door") and troll4 == False:
-			print "The door is blocked by a troll.\n"
+			print("The door is blocked by a troll.\n")
+		elif action in ("leave"):
+			print("You can either go through the door or the dark doorway.")
 		elif action in ("doorway", "dark doorway"):
 			room2()
+		elif action in ("stats", "health"):
+			print(player)
+		elif action in ("inventory"):
+			print(inventory)
 		else:
-			print "You can't do that here.\n"
+			print("You can't do that here.\n")
 	room5()
 	
 # Fifth room
@@ -204,19 +223,23 @@ def room5():
 	# initialize action
 	action = ""
 	# room intro
-	print "You enter a room with a treasure chest.\n"
+	print("You enter a room with a treasure chest.\n")
 	# loop until user leaves room
 	while action not in ("chest", "open chest", "treasure chest", "take treasure chest", "open treasure chest", "take chest", "look in chest", "look in treasure chest"):
 		# ask user for action
-		action = raw_input("What do you do? ").lower()
+		action = input("What do you do? ").lower()
 		if action in ("door", "leave", "go through door"):
 			room4()
 		elif action in ('look around', 'look', 'survey', 'look at room'):
-			print "A room containing only a treasure chest.\n"
+			print("A room containing only a treasure chest.\n")
+		elif action in ("stats", "health"):
+			print(player)
+		elif action in ("inventory"):
+			print(inventory)
 		elif action not in ("door", "leave", "go through door"):
-			print "You can't do that here.\n"
-	print "You found the Golden Chalice!"
-	action = raw_input("Thank you for playing Dungeon Quest. To play again, press Enter. To quit, enter Quit.").lower()
+			print("You can't do that here.\n")
+	print("You found the Golden Chalice!")
+	action = input("Thank you for playing Dungeon Quest. To play again, press Enter. To quit, enter Quit.").lower()
 	# restart
 	if action != "quit":
 		start()
